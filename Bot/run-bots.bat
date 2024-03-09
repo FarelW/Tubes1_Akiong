6019212a612b478bfa0bd1c5d1969094dc8f4b91
FarelW <eeksapi321@gmail.com>
2024-03-09 22:26:03 +0700

    update: Akiongbot final

--- Bot/run-bots.bat
+++ Bot/run-bots.bat
@@ -1,6 +1,6 @@
 @echo off
 start cmd /c "python main.py --logic V1 --email=cobacobabot@gmail.com --name=gdistance --password=123456 --team etimo"
+start cmd /c "python main.py --logic Akiong --email=cobacobabottttt@gmail.com --name=akiongbot --password=123456 --team etimo"
 start cmd /c "python main.py --logic V2 --email=cobacobabott@gmail.com --name=gvalue --password=123456 --team etimo"
 start cmd /c "python main.py --logic V3 --email=cobacobabottt@gmail.com --name=gdiamond --password=123456 --team etimo"
 start cmd /c "python main.py --logic V4 --email=cobacobabotttt@gmail.com --name=gbase --password=123456 --team etimo"
-start cmd /c "python main.py --logic Akiong --email=cobacobabottttt@gmail.com --name=akiongbot --password=123456 --team etimo"
